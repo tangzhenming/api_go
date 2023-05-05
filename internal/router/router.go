accf352876c5c9c33954a125644ab4b28720afbd
tang <15977536060@163.com>
2023-05-05 08:25:24 +0000

    refactor: 拆分出 router 和 controller

---
+++ internal/router/router.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tang-projects/api_go/internal/controller"
+)
+
+func Serve() {
+	r := gin.Default()
+	r.GET("/ping", controller.Ping)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
