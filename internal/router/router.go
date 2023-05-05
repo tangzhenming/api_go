693f6b296080cc4463448ef10ef65210f67474df
tang <15977536060@163.com>
2023-05-05 10:09:58 +0000

    chore: change name

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,7 +5,7 @@ import (
 	"github.com/tang-projects/api_go/internal/controller"
 )
 
-func Serve() {
+func RunServe() {
 	r := gin.Default()
 	r.GET("/ping", controller.Ping)
 	r.Run() // listen and serve on 0.0.0.0:8080
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/tang-projects/api_go/internal/router"
 
 func main() {
-	router.Serve()
+	router.RunServe()
 }
