dfae61f35707e637cea4e487e586640bcf889601
tang <15977536060@163.com>
2023-05-13 05:46:21 +0000

    feat: 为 User 表添加字段

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -4,6 +4,7 @@ import "time"
 
 type User struct {
 	UserID     int       `gorm:"primary_key;autoIncrement" json:"userID"`
+	UserMail   string    `json:"userMail"`
 	UserName   string    `json:"userName"`
 	CreateTime time.Time `json:"createTime"`
 }
