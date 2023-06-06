f7344514588dfa9909303027b033dca1f6f3bb2d
tang <15977536060@163.com>
2023-06-06 03:54:57 +0000

    refactor: Use a whitelist to optimize the code. Use closure to capture parameter for reduce the repitition of it

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tang-projects/api_go/internal/utils"
 )
 
 func Run(port string) {
 	r := gin.Default()
 
-	setupUser(r)
+	setupUserRoutes(r)
 
 	r.Run(fmt.Sprintf(":%s", port))
+}
+
+// 路由 AuthMiddleware 权限控制
+// Use a whitelist to optimize the code
+func AddAuthRoute(r *gin.Engine, method string, path string, handler gin.HandlerFunc, auth bool) {
+	if auth {
+		r.Handle(method, path, utils.AuthMiddleware, handler)
+	} else {
+		r.Handle(method, path, handler)
+	}
 }
--- internal/router/routes.go
+++ internal/router/routes.go
@@ -3,15 +3,19 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tang-projects/api_go/internal/controllers"
-	"github.com/tang-projects/api_go/internal/utils"
 )
 
-func setupUser(r *gin.Engine) {
+func setupUserRoutes(r *gin.Engine) {
 	ctrl := controllers.UserController{}
 
-	r.POST("api/v1/users", ctrl.CreateUser)
-	r.GET("api/v1/users/:id", utils.AuthMiddleware, ctrl.ReadUser)
-	r.PUT("api/v1//users/:id", utils.AuthMiddleware, ctrl.UpdateUser)
-	r.DELETE("api/v1//users/:id", utils.AuthMiddleware, ctrl.DeleteUser) // 谨慎提供给用户，一般来说不会开放给用户进行操作
-	r.POST("api/v1/users/logout", utils.AuthMiddleware, ctrl.LogoutUser)
+	// One way to reduce the repetition of the r parameter and the common path is to defined a closure within this function that captures the parameters and paths and calls the AddAuthRoute function with it
+	addAuthUserRoute := func(method string, path string, handler gin.HandlerFunc, auth bool) {
+		basePath := "api/v1/users"
+		AddAuthRoute(r, method, basePath+path, handler, auth)
+	}
+	addAuthUserRoute("POST", "", ctrl.CreateUser, false)
+	addAuthUserRoute("GET", "/:id", ctrl.ReadUser, true)
+	addAuthUserRoute("PUT", "/:id", ctrl.UpdateUser, true)
+	addAuthUserRoute("DELETE", "/:id", ctrl.DeleteUser, true) // 谨慎提供给用户，一般来说不会开放给用户进行操作
+	addAuthUserRoute("POST", "/logout", ctrl.LogoutUser, true)
 }
