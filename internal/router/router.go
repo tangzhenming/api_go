f64a7719f2f060f65a63ba17eb42c2312aed5efb
tang <15977536060@163.com>
2023-06-26 08:29:58 +0000

    fix: 开发环境和生产环境分别配置跨域

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -12,8 +14,9 @@ func Run(port string) {
 	r := gin.Default()
 
 	// 配置 CORS 策略
+	// 使用 os.Getenv 函数来获取 CORS_ALLOW_ORIGINS 环境变量的值，并使用 strings.Split 函数将其分割为一个字符串切片。然后我们将这个字符串切片赋值给 AllowOrigins 字段。在本地开发环境中，可以将 CORS_ALLOW_ORIGINS 环境变量设置为 "http://localhost:3000"，以允许来自本地前端应用程序的跨域请求。在生产环境中，可以将 CORS_ALLOW_ORIGINS 环境变量设置为 "http://cra.tangzhenming.com"，以允许来自生产环境前端应用程序的跨域请求。
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://cra.tangzhenming.com"}
+	config.AllowOrigins = strings.Split(os.Getenv("CORS_ALLOW_ORIGINS"), ",")
 	r.Use(cors.New(config))
 
 	setupUserRoutes(r)
