7b661af4e404716dcb80077ae55a5948a86ecdc1
tang <15977536060@163.com>
2023-06-26 08:04:38 +0000

    docs: update

--- internal/utils/email.go
+++ internal/utils/email.go
@@ -43,6 +43,8 @@ func SendEmail(email string) (string, error) {
 
 	// 配置 TLS 连接
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	// TLS（传输层安全协议）是一种用于在计算机网络上提供安全通信的协议。它可以在客户端和服务器之间建立一个安全的加密连接，以保护传输的数据不被窃听或篡改。
+	// 当您的应用程序需要与其他服务器（例如电子邮件服务器、Web 服务器或 API 服务器）建立安全连接时，您需要配置 TLS 连接。配置 TLS 连接可以让您的应用程序验证服务器证书的有效性，并使用加密算法来保护传输的数据。
 
 	// Send the email
 	if err = d.DialAndSend(m); err != nil {
