a4fa49212a407a155f29a91d3006ee7d2f9dd874
tang <15977536060@163.com>
2023-07-05 03:22:40 +0000

    chore: 移除冗余代码

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -15,9 +15,9 @@ type User struct {
 }
 
 type ResponseUser struct {
-	ID        uint      `json:"ID"`
-	CreatedAt time.Time `json:"CreatedAt"`
-	UpdatedAt time.Time `json:"UpdatedAt"`
-	Name      string    `json:"Name"`
-	Email     string    `json:"Email"`
+	ID        uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+	Email     string
 }
